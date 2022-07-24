2f6a3b58eb14d547f31f72604af8bdcee055fe41
ctl <3054008136@qq.com>
2022-07-24 19:46:17 +0800

    feat(skus): 规格管理

---
+++ src/views/skus/columns.js
@@ -0,0 +1,29 @@
+export const columns = [
+  {
+    title: '规格名称',
+    dataIndex: 'name',
+    key: 'name'
+  },
+  {
+    title: '规格值',
+    dataIndex: 'default',
+    key: 'default'
+  },
+  {
+    title: '排序',
+    dataIndex: 'order',
+    key: 'order',
+    width: 100
+  },
+  {
+    title: '状态',
+    dataIndex: 'status',
+    key: 'status',
+    width: 100
+  },
+  {
+    title: '操作',
+    dataIndex: 'operating',
+    key: 'operating'
+  }
+]
